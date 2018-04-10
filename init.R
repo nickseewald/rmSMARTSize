e7fc21ec33212c3618b39e48357ca3a58cad74f3
Nick Seewald <nseewald@umich.edu>
2018-04-10 12:15:02 -0400

    initial move towards system-independent cluster creation

--- init.R
+++ init.R
@@ -1,9 +1,40 @@
-library(xtable)
 library(MASS)
+library(doParallel)
+library(doRNG)
+library(slackr)
+library(xtable)
+
 wd <- getwd()
-setwd("C:\\Users\\nseew\\Documents\\GitHub\\rmSMARTSize")
-source("functions.R")
-source("generateSMART.R")
-source("simulateSMART.R")
-setwd(wd)
-rm(wd)
+
+source(paste0(wd, "/functions.R"))
+source(paste0(wd, "/generateSMART.R"))
+source(paste0(wd, "/simulateSMART.R"))
+
+slackrSetup(config_file = paste0(wd, "/d3slack.dcf"))
+
+### CLUSTER SETUP ###
+nodefile <- Sys.getenv("PBS_NODEFILE")
+# Check for Flux and create cluster appropriately
+if (nodefile != "") {
+  hostlist <- read.table(nodefile, skip = 1, header = FALSE)
+  clus <- makeCluster(c(as.character(hostlist$V1)))
+} else {
+  ncore <- detectCores()
+  clus <- makeCluster(ncore)
+}
+
+# Set up cluster for parallel computation
+
+clusterExport(clus, varlist = "wd")
+clusterEvalQ(clus, {
+  library(MASS)
+  library(doParallel)
+  library(doRNG)
+  library(slackr)
+  library(xtable)
+  
+  source(paste0(wd, "/functions.R"), echo = F)
+  source(paste0(wd, "/generateSMART.R"), echo = F)
+  source(paste0(wd, "/simulateSMART.R"), echo = F)
+})
+registerDoParallel(clus)
