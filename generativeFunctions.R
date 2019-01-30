c1d097864fc9933840c5c27a56072c9674c84798
Nick Seewald <nseewald@umich.edu>
2019-01-30 15:48:08 -0500

    Stage 2 mean generation for design 2

--- generativeFunctions.R
+++ generativeFunctions.R
@@ -6,6 +6,8 @@ generateStage1 <- function(n, times, spltime, r1, r0, gammas,
                             respFunction, respDirection = NULL, 
                             balanceRand = FALSE,
                             empirical = FALSE, old = FALSE) {
+  if (old) 
+    warning("Option 'old' currently does nothing in generateStage1()")
   
   corstr <- match.arg(corstr)
   
@@ -55,12 +57,16 @@ generateStage1 <- function(n, times, spltime, r1, r0, gammas,
   d$R[d$A1 == 1]  <- d$R.1[d$A1 == 1]
   d$R[d$A1 == -1] <- d$R.0[d$A1 == -1]
   
+  class(d) <- c("stage1SMART", class(d))
   d
 }
 
 generateStage2.means <- function(d, times, spltime, gammas, lambdas,
                                  design, corstr = c("identity", "exchangeable", "ar1")) {
+  if (!("stage1SMART") %in% class(d))
+    stop("'d' must be of class 'stage1SMART'")
   
+  n <- nrow(d)
   nDTR <- switch(design, 8, 4, 3)
   DTRs <- dtrIndex(design)
   
@@ -70,12 +76,16 @@ generateStage2.means <- function(d, times, spltime, gammas, lambdas,
   
   dtrTxts <- do.call(rbind, DTRs)
   
+  # Replicate the "potential" response status variables for each DTR,
+  # so that each DTR gets the appropriate potential response
   respMatrix <- do.call(cbind, lapply((1+dtrTxts[1, ])/2, function(a1) d[[paste0("R.", a1)]]))
   
   # Create a vector of response probabilities, each entry of which corresponds to 
   # the appropriate value (r0 or r1) for each DTR
   respProbVec <- sapply(paste0("r", (1+dtrTxts[1, ])/2), get)
   
+  # Create matrices of the "adjustments" added to the mean model that are 
+  # specific to responders and non-responders
   if (design == 1) {
     designMeanAdj.R <- matrix(rep(
       dtrTxts[2, ] * (gammas[6] + gammas[8] * dtrTxts[1, ]) / respProbVec +
@@ -85,7 +95,17 @@ generateStage2.means <- function(d, times, spltime, gammas, lambdas,
       dtrTxts[3, ] * (gammas[7] + gammas[9] * dtrTxts[1, ]) / (1 - respProbVec) -
         respProbVec * (lambdas[1] + lambdas[2] * dtrTxts[1, ]),
       n), nrow = n, byrow = T)
-  }
+  } else if (design == 2) {
+    designMeanAdj.R <- matrix(rep(
+      (1 - respProbVec) * (lambdas[1] + lambdas[2] * dtrTxts[1, ]),
+      n), nrow = n, byrow = T)
+    designMeanAdj.NR <- matrix(rep(
+      dtrTxts[3, ] * (gammas[6] + gammas[7] * dtrTxts[1, ]) / (1 - respProbVec) -
+        respProbVec * (lambdas[1] + lambdas[2] * dtrTxts[1, ]),
+      n), nrow = n, byrow = T)
+  } else if (design == 3) {
+    
+  } else stop("'design' must be one of 1, 2, or 3.")
   
   if (corstr %in% c("identity", "exchangeable")) {
     # FIXME: This only works for time 2 right now! Any other time will not have proper correlation structure
@@ -93,43 +113,18 @@ generateStage2.means <- function(d, times, spltime, gammas, lambdas,
       # Start with a matrix of just the appropriate linear combinations of gammas
       # (only up to the stage 1 parts of the time 2 model)
       matrix(rep(
-        ((1-rho) / (1+rho)) * gammas[1] + (gammas[2] + gammas[3] * dtrTxts[1, ])/(1+rho) +
+        ((1-rho)/(1+rho)) * gammas[1] + (gammas[2] + gammas[3] * dtrTxts[1, ])/(1+rho) +
           gammas[4] + gammas[5] * dtrTxts[1, ],
         n), nrow = n, byrow = T) + 
       # Add Y0 and the appropriate causal Y1
-      (rho/(1+rho)) * (d$Y0 + matrix(c(rep(d$Y1.1, 4), rep(d$Y1.0, 4)), nrow = n)) +
-      # Add 
+      (rho/(1+rho)) * (d$Y0 + 
+                         matrix(c(rep(d$Y1.1, sum(dtrTxts[1, ] == 1)),
+                                      rep(d$Y1.0, sum(dtrTxts[1, ] == -1))), nrow = n)
+                       ) +
+      # Add response adjustment
       respMatrix * designMeanAdj.R + (1 - respMatrix) * designMeanAdj.NR
-    
-    
-
-    
   } else if (corstr == "ar1") {
     stop("generateStage2 not yet implemented for corstr = 'ar1'")
   }
-  
-  
-  # for (i in 1:nDTR){
-  #   d[[paste0("Y2.", dtrTriples[1])]] <- 
-  # }
-  
-  
-  ## Time 2 means
-  d$Y2.11 <- (1-rho)/(1+rho) * gammas[1] + (gammas[2] + gammas[3]) / (1+rho) +
-    rho/(1+rho) * (d$Y0 + d$Y1.1) + gammas[4] + gammas[5] + (1-d$R.1)*(gammas[6] + gammas[7])/(1-r1) +
-    (d$R.1 - r1) * (lambdas[1] + lambdas[2]) 
-  
-  d$Y2.10 <- (1-rho)/(1+rho) * gammas[1] + (gammas[2] + gammas[3]) / (1+rho) +
-    rho/(1+rho) * (d$Y0 + d$Y1.1) + gammas[4] + gammas[5] + (1-d$R.1)*(-gammas[6] - gammas[7])/(1-r1) +
-    (d$R.1 - r1) * (lambdas[1] + lambdas[2])
-  
-  d$Y2.01 <- (1-rho)/(1+rho) * gammas[1] + (gammas[2] - gammas[3]) / (1+rho) +
-    rho/(1+rho) * (d$Y0 + d$Y1.0) + gammas[4] - gammas[5] + (1-d$R.0)*(gammas[6] - gammas[7])/(1-r0) +
-    (d$R.0 - r0) * (lambdas[1] - lambdas[2])
-  
-  d$Y2.00 <- (1-rho)/(1+rho) * gammas[1] + spltime * (gammas[2] - gammas[3]) / (1+rho) +
-    rho/(1+rho) * (d$Y0 + d$Y1.0) + gammas[4] - gammas[5] + (1-d$R.0)*(-gammas[6] + gammas[7])/(1-r0) +
-    (d$R.0 - r0) * (lambdas[1] - lambdas[2])
-  
-  
+  d
 }
