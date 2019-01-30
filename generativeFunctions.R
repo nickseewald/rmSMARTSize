7b61d2ef33fde57d2ccdfabac71e818a56bf30da
Nick Seewald <nseewald@umich.edu>
2019-01-29 23:32:46 -0500

    Initial pass at generating time-2 means more generally

--- generativeFunctions.R
+++ generativeFunctions.R
@@ -57,3 +57,79 @@ generateStage1 <- function(n, times, spltime, r1, r0, gammas,
   
   d
 }
+
+generateStage2.means <- function(d, times, spltime, gammas, lambdas,
+                                 design, corstr = c("identity", "exchangeable", "ar1")) {
+  
+  nDTR <- switch(design, 8, 4, 3)
+  DTRs <- dtrIndex(design)
+  
+  dtrTriples <- as.data.frame((do.call(cbind, DTRs) + 1) / 2)
+  dtrTriples[dtrTriples == 0.5] <- 0
+  dtrTriples <- apply(dtrTriples, 1, function(x) paste(x, collapse = ""))
+  
+  dtrTxts <- do.call(rbind, DTRs)
+  
+  respMatrix <- do.call(cbind, lapply((1+dtrTxts[1, ])/2, function(a1) d[[paste0("R.", a1)]]))
+  
+  # Create a vector of response probabilities, each entry of which corresponds to 
+  # the appropriate value (r0 or r1) for each DTR
+  respProbVec <- sapply(paste0("r", (1+dtrTxts[1, ])/2), get)
+  
+  if (design == 1) {
+    designMeanAdj.R <- matrix(rep(
+      dtrTxts[2, ] * (gammas[6] + gammas[8] * dtrTxts[1, ]) / respProbVec +
+        (1 - respProbVec) * (lambdas[1] + lambdas[2] * dtrTxts[1, ]),
+      n), nrow = n, byrow = T)
+    designMeanAdj.NR <- matrix(rep(
+      dtrTxts[3, ] * (gammas[7] + gammas[9] * dtrTxts[1, ]) / (1 - respProbVec) -
+        respProbVec * (lambdas[1] + lambdas[2] * dtrTxts[1, ]),
+      n), nrow = n, byrow = T)
+  }
+  
+  if (corstr %in% c("identity", "exchangeable")) {
+    # FIXME: This only works for time 2 right now! Any other time will not have proper correlation structure
+    d[, paste0("Y", times[times > spltime], ".", dtrTriples)] <- 
+      # Start with a matrix of just the appropriate linear combinations of gammas
+      # (only up to the stage 1 parts of the time 2 model)
+      matrix(rep(
+        ((1-rho) / (1+rho)) * gammas[1] + (gammas[2] + gammas[3] * dtrTxts[1, ])/(1+rho) +
+          gammas[4] + gammas[5] * dtrTxts[1, ],
+        n), nrow = n, byrow = T) + 
+      # Add Y0 and the appropriate causal Y1
+      (rho/(1+rho)) * (d$Y0 + matrix(c(rep(d$Y1.1, 4), rep(d$Y1.0, 4)), nrow = n)) +
+      # Add 
+      respMatrix * designMeanAdj.R + (1 - respMatrix) * designMeanAdj.NR
+    
+    
+
+    
+  } else if (corstr == "ar1") {
+    stop("generateStage2 not yet implemented for corstr = 'ar1'")
+  }
+  
+  
+  # for (i in 1:nDTR){
+  #   d[[paste0("Y2.", dtrTriples[1])]] <- 
+  # }
+  
+  
+  ## Time 2 means
+  d$Y2.11 <- (1-rho)/(1+rho) * gammas[1] + (gammas[2] + gammas[3]) / (1+rho) +
+    rho/(1+rho) * (d$Y0 + d$Y1.1) + gammas[4] + gammas[5] + (1-d$R.1)*(gammas[6] + gammas[7])/(1-r1) +
+    (d$R.1 - r1) * (lambdas[1] + lambdas[2]) 
+  
+  d$Y2.10 <- (1-rho)/(1+rho) * gammas[1] + (gammas[2] + gammas[3]) / (1+rho) +
+    rho/(1+rho) * (d$Y0 + d$Y1.1) + gammas[4] + gammas[5] + (1-d$R.1)*(-gammas[6] - gammas[7])/(1-r1) +
+    (d$R.1 - r1) * (lambdas[1] + lambdas[2])
+  
+  d$Y2.01 <- (1-rho)/(1+rho) * gammas[1] + (gammas[2] - gammas[3]) / (1+rho) +
+    rho/(1+rho) * (d$Y0 + d$Y1.0) + gammas[4] - gammas[5] + (1-d$R.0)*(gammas[6] - gammas[7])/(1-r0) +
+    (d$R.0 - r0) * (lambdas[1] - lambdas[2])
+  
+  d$Y2.00 <- (1-rho)/(1+rho) * gammas[1] + spltime * (gammas[2] - gammas[3]) / (1+rho) +
+    rho/(1+rho) * (d$Y0 + d$Y1.0) + gammas[4] - gammas[5] + (1-d$R.0)*(-gammas[6] + gammas[7])/(1-r0) +
+    (d$R.0 - r0) * (lambdas[1] - lambdas[2])
+  
+  
+}
